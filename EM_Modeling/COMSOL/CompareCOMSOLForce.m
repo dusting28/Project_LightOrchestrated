3537eb0d808834b3c957473ea0be57b739a732fa
dusting28 <dusting28@yahoo.com>
2025-04-17 10:39:27 -0700

    Created figures 1-3 and rendered PCB CAD

---
+++ EM_Modeling/COMSOL/CompareCOMSOLForce.m
@@ -0,0 +1,53 @@
+%% By: Dustin Goetz
+clear; clc, close all;
+addpath("COMSOL_Data")
+
+
+%% Static Forces
+inductorID = "32";
+movingMass = "Magnet3x4_Shielded";
+comsol_data = readmatrix(strcat("COMSOL_Data/Inductor",inductorID,"_",movingMass,".csv"));
+
+x = comsol_data(1:3:end,1);
+force = zeros(3,length(x));
+for iter1 = 1:3
+    force(iter1,:) = 1000*comsol_data(iter1:3:end,3)';
+end
+
+coil_force_1 = force(1,:)-force(2,:);
+core_force_1 = -force(2,:);
+total_force_1 = force(1,:);
+
+inductorID = "32";
+movingMass = "Magnet3x4_Design8";
+comsol_data = readmatrix(strcat("COMSOL_Data/Inductor",inductorID,"_",movingMass,".csv"));
+
+x = comsol_data(1:3:end,1);
+force = zeros(3,length(x));
+for iter1 = 1:3
+    force(iter1,:) = 1000*comsol_data(iter1:3:end,3)';
+end
+
+coil_force_2 = force(1,:)-force(2,:);
+core_force_2 = -force(2,:);
+total_force_2 = force(1,:);
+
+figure;
+plot(x(2:end),core_force_1(2:end));
+hold on;
+plot(x(2:end),coil_force_1(2:end));
+
+figure;
+plot(x(2:end),core_force_2(2:end));
+hold on;
+plot(x(2:end),coil_force_2(2:end));
+
+figure;
+plot(x(2:end),coil_force_1(2:end));
+hold on;
+plot(x(2:end),coil_force_2(2:end));
+
+figure;
+plot(x(2:end),total_force_1(2:end));
+hold on;
+plot(x(2:end),total_force_2(2:end));
